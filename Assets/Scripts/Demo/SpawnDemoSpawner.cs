e177471e980a66f6074391aa04f97d2813de4493
Reese Schultz <reeses@gatech.edu>
2020-01-24 23:18:14 -0500

    Add materials and update demos for usability

--- Assets/Scripts/Demo/SpawnDemoSpawner.cs
+++ Assets/Scripts/Demo/SpawnDemoSpawner.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using Reese.Nav;
+using UnityEngine;
 using UnityEngine.UI;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace Reese.Demo
 {
     class SpawnDemoSpawner : MonoBehaviour
     {
+        public bool IsForAgents;
         public Button Button;
         public Text SpawnText;
         public Slider Slider;
@@ -27,20 +31,35 @@ namespace Reese.Demo
 
             SpawnText.text = "Spawn " + enqueueCount;
 
-            if (enqueueCount == 1) SpawnText.text += " Person";
-            else SpawnText.text += " People";
+            if (enqueueCount == 1) SpawnText.text += " Entity";
+            else SpawnText.text += " Entities";
         }
 
         void enqueue()
         {
             if (Button == null) return;
 
-            PersonSpawnSystem.Enqueue(new PersonSpawn
+            if (!IsForAgents) PersonSpawnSystem.Enqueue(new PersonSpawn
             {
                 Person = new Person
                 {
                     RandomizeTranslation = true
                 }
+            }, enqueueCount);
+            else NavSpawnSystem.Enqueue(new NavAgentSpawn
+            {
+               Agent = new NavAgent
+                {
+                    JumpDegrees = 45,
+                    JumpGravity = 200,
+                    TranslationSpeed = 20,
+                    TypeID = NavUtil.GetAgentType(NavConstants.HUMANOID),
+                    Offset = new float3(0, 1, 0)
+                },
+                Translation = new Translation
+                {
+                    Value = new float3(0, 1, 0)
+                } 
             }, enqueueCount);
         }
     }
