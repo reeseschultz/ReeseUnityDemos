b0bbd10014e8f186433b541653287e1b117400ec
Reese Schultz <reeses@gatech.edu>
2020-01-27 09:37:11 -0500

    Update NavQueueSystem with reduced public surface

--- Assets/Scripts/Nav/Agent/NavQueueSystem.cs
+++ Assets/Scripts/Nav/Agent/NavQueueSystem.cs
@@ -5,8 +5,8 @@ using Unity.Jobs;
 
 namespace Reese.Nav
 {
-    /// <summary>Thread-safe system for enqueuing and dequeuing NavAgents for
-    /// path and jump planning in the NavPlanSystem.</summary>
+    /// <summary>Thread-safe system for dequeuing NavAgents for path and jump
+    /// planning.</summary>
     class NavQueueSystem : JobComponentSystem
     {
         /// <summary>The path planning queue.</summary>
@@ -15,20 +15,12 @@ namespace Reese.Nav
         /// <summary>The jump planning queue.</summary>
         static ConcurrentQueue<Entity> jumpPlanningQueue = new ConcurrentQueue<Entity>();
 
-        /// <summary>Enqueues an entity for path planning.</summary>
-        public static void EnqueuePathPlanning(Entity entity)
-            => pathPlanningQueue.Enqueue(entity);
-
         /// <summary>Dequeues an entity for path planning.</summary>
         public static Entity DequeuePathPlanning()
         {
             pathPlanningQueue.TryDequeue(out Entity entity);
             return entity;
         }
-
-        /// <summary>Enqueues an entity for jump planning.</summary>
-        public static void EnqueueJumpPlanning(Entity entity)
-            => jumpPlanningQueue.Enqueue(entity);
 
         /// <summary>Dequeues an entity for jump planning.</summary>
         public static Entity DequeueJumpPlanning()
@@ -40,11 +32,11 @@ namespace Reese.Nav
         protected override JobHandle OnUpdate(JobHandle inputDeps)
             => Entities
                 .ForEach((Entity entity, ref NavAgent agent) => {
-                    if (agent.IsJumping) EnqueueJumpPlanning(entity);
+                    if (agent.IsJumping) jumpPlanningQueue.Enqueue(entity);
                     else if (!agent.HasQueuedPathPlanning && agent.HasDestination)
                     {
                         agent.HasQueuedPathPlanning = true;
-                        EnqueuePathPlanning(entity);
+                        pathPlanningQueue.Enqueue(entity);
                     }
                 })
                 .WithName("NavQueueJob")
