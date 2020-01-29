b03259f5b8878c790807adfa6aa3c3da2806a8ea
Reese Schultz <reeses@gatech.edu>
2020-01-29 10:40:04 -0500

    Update nav user guide and NavAgentStatus docs

--- Assets/Scripts/Nav/Agent/NavAgentStatus.cs
+++ Assets/Scripts/Nav/Agent/NavAgentStatus.cs
@@ -2,9 +2,20 @@
 
 namespace Reese.Nav
 {
+    /// <summary>Exists if the agent is falling.</summary>
     struct NavFalling : IComponentData { }
+
+    /// <summary>Exists if the agent is jumping.</summary>
     struct NavJumping : IComponentData { }
+
+    /// <summary>Exists if the agent  recently jumped, *past tense*, primarily
+    /// so the NavSurfaceSystem can raycast for a new surface potentially
+    /// underneath said agent.</summary>
     struct NavJumped : IComponentData { }
+
+    /// <summary>Exists if the agent is lerping.</summary>
     struct NavLerping : IComponentData { }
+
+    /// <summary>Exists if the agent is planning paths or jumps.</summary>
     struct NavPlanning : IComponentData { }
 }
