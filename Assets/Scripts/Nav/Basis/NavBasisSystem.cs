6bc78dd15b9a7edc1296c0ca265ecdcf304f79c6
Reese Schultz <reeses@gatech.edu>
2020-01-30 10:45:41 -0500

    Update nav comments for readability and correctness

--- Assets/Scripts/Nav/Basis/NavBasisSystem.cs
+++ Assets/Scripts/Nav/Basis/NavBasisSystem.cs
@@ -33,9 +33,9 @@ namespace Reese.Nav
         {
             var commandBuffer = barrier.CreateCommandBuffer().ToConcurrent();
 
-            // Below job is needed because Unity.Physics can remove the Parent
-            // component, at least in 2019.3, thus it has to be added later at
-            // runtime and not in authoring. Please submit an issue or PR if
+            // Below job is needed because Unity.Physics has been observed to remove
+            // the Parent component, thus it can only reliably be added later at
+            // runtime and not in authoring :(. Please submit an issue or PR if
             // you've a cleaner solution.
             var addParentJob = Entities
                 .WithNone<Parent>()
--- Assets/Scripts/Nav/Surface/NavSurfaceSystem.cs
+++ Assets/Scripts/Nav/Surface/NavSurfaceSystem.cs
@@ -38,9 +38,9 @@ namespace Reese.Nav
             var commandBuffer = barrier.CreateCommandBuffer().ToConcurrent();
             var defaultBasis = World.GetExistingSystem<NavBasisSystem>().DefaultBasis;
 
-            // Below job is needed because Unity.Physics can remove the Parent
-            // component, at least in 2019.3, thus it has to be added later at
-            // runtime and not in authoring. Please submit an issue or PR if
+            // Below job is needed because Unity.Physics has been observed to remove
+            // the Parent component, thus it can only reliably be added later at
+            // runtime and not in authoring :(. Please submit an issue or PR if
             // you've a cleaner solution.
             var addParentJob = Entities
                 .WithNone<Parent>()
