ff090ce1a97ac633b674610cd9b8cae2c7be605c
William <2007359+williamtsoi@users.noreply.github.com>
2020-07-09 19:24:52 +0100

    Add GroundedSystem to set agent height and rotation based on surface

---
+++ Packages/com.reese.nav/Agent/NavGroundedAgentSystem.cs
@@ -0,0 +1,58 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using Unity.Transforms;
+
+namespace Reese.Nav
+{
+    [UpdateAfter(typeof(NavInterpolationSystem))]
+    public class NavGroundedAgentSystem : SystemBase
+    {
+        BuildPhysicsWorld buildPhysicsWorld => World.GetExistingSystem<BuildPhysicsWorld>();
+
+        protected override void OnUpdate()
+        {
+            var physicsWorld = buildPhysicsWorld.PhysicsWorld;
+
+            Dependency = JobHandle.CombineDependencies(Dependency, buildPhysicsWorld.FinalJobHandle);
+
+            Entities
+               .WithNone<NavPlanning, NavJumping>()
+               .WithAll<NavLerping, LocalToParent>()
+               .WithReadOnly(physicsWorld)
+               .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref Rotation rotation, in NavAgent agent, in LocalToWorld localToWorld, in Parent surface) =>
+               {
+                   var rayInput = new RaycastInput
+                   {
+                       Start = localToWorld.Position + agent.Offset,
+                       End = -localToWorld.Up * NavConstants.SURFACE_RAYCAST_DISTANCE_MAX,
+                       Filter = new CollisionFilter()
+                       {
+                           BelongsTo = NavUtil.ToBitMask(NavConstants.COLLIDER_LAYER),
+                           CollidesWith = NavUtil.ToBitMask(NavConstants.SURFACE_LAYER),
+                       }
+                   };
+
+                   if (physicsWorld.CastRay(rayInput, out RaycastHit hit))
+                   {
+                       // Rotate entity so it is level with the surface it is on.
+                       //var currentRotation = rotation.Value;
+                       //var up = hit.SurfaceNormal;
+                       //var rotationUp = quaternion.EulerXYZ(up.x, 0, up.z);
+                       //currentRotation = math.mul(rotationUp, currentRotation);
+
+                       //rotation.Value = currentRotation;
+
+                       var currentPosition = translation.Value;
+                       currentPosition.y = hit.Position.y + agent.Offset.y;
+
+                       translation.Value = currentPosition;
+                   }
+               })
+               .WithName("Set_grounded_height_and_rotation")
+               .ScheduleParallel();
+        }
+    }
+}
