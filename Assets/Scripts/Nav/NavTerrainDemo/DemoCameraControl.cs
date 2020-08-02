f7bcbcbe95293147792585cd82bb7e79b1e50f42
William <2007359+williamtsoi@users.noreply.github.com>
2020-08-02 21:10:25 +0100

    Added simple camera movement

---
+++ Assets/Scripts/Nav/NavTerrainDemo/DemoCameraControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCameraControl : MonoBehaviour
+{
+    [SerializeField]
+    public float cameraTraverseSpeed = 100f;
+
+    [SerializeField]
+    public float cameraRotateSpeed = 100f;
+
+    void Update()
+    {
+        Vector3 cameraMovement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * cameraRotateSpeed * Time.deltaTime, Space.World);
+            transform.Rotate(-Input.GetAxis("Mouse Y") * cameraRotateSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            cameraMovement += transform.forward * cameraTraverseSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            cameraMovement -= transform.forward * cameraTraverseSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            cameraMovement += transform.right * cameraTraverseSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            cameraMovement -= transform.right * cameraTraverseSpeed * Time.deltaTime;
+        }
+
+        transform.position += cameraMovement;
+    }
+}
