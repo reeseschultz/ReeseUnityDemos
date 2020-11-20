cf824b9f6ea84133bcabce833ce0d3171f153092
Reese Schultz <reeseschultz@gmail.com>
2020-11-19 22:50:45 -0500

    Parameterize and add NavRotator to Stranded demo scene

--- Assets/Scripts/Nav/NavRotator.cs
+++ Assets/Scripts/Nav/NavRotator.cs
@@ -5,10 +5,19 @@ namespace Reese.Demo
 {
     class NavRotator : MonoBehaviour
     {
+        [SerializeField]
         float xAngleMax = 10;
+
+        [SerializeField]
         float yAngleMax = 30;
 
+        [SerializeField]
+        float xAngleRate = 0.5f;
+
+        [SerializeField]
+        float yAngleRate = 2;
+
         void Update()
-            => transform.rotation = Quaternion.Euler(xAngleMax * math.sin(Time.time * 0.5f), 0, yAngleMax * math.sin(Time.time * 2));
+            => transform.rotation = Quaternion.Euler(xAngleMax * math.sin(Time.time * xAngleRate), 0, yAngleMax * math.sin(Time.time * yAngleRate));
     }
 }
