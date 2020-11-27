4d486684194489a7853652b946d575d9f9b7fe0c
Reese Schultz <reeseschultz@gmail.com>
2020-11-27 12:21:49 -0500

    Limit which surfaces are jumpable from one another in Stranded demo

--- Assets/Scripts/Stranded/StrandedDestinationSystem.cs
+++ Assets/Scripts/Stranded/StrandedDestinationSystem.cs
@@ -4,12 +4,16 @@ using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using Unity.Physics;
+using Unity.Physics.Systems;
 
 namespace Reese.Demo
 {
     [UpdateAfter(typeof(TransformSystemGroup))]
     class StrandedDestinationSystem : SystemBase
     {
+        PhysicsWorld physicsWorld => World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<BuildPhysicsWorld>().PhysicsWorld;
+
         public GameObject AgentTransformGameObject { get; private set; }
 
         Entity agentEntity;
@@ -48,17 +52,66 @@ namespace Reese.Demo
 
             var mouse = Mouse.current;
 
+            var screenPointToRay = Camera.main.ScreenPointToRay(
+                new Vector3(
+                    mouse.position.x.ReadValue(),
+                    mouse.position.y.ReadValue()
+                )
+            );
+
+            var rayInput = new RaycastInput
+            {
+                Start = screenPointToRay.origin,
+                End = screenPointToRay.GetPoint(500),
+                Filter = CollisionFilter.Default
+            };
+
             if (
                 mouse == null ||
                 !mouse.leftButton.isPressed ||
-                !Physics.Raycast(Camera.main.ScreenPointToRay(new Vector2(mouse.position.x.ReadValue(), mouse.position.y.ReadValue())), out RaycastHit hit)
+                !physicsWorld.CastRay(rayInput, out var hit)
             ) return;
 
-            EntityManager.AddComponentData(agentEntity, new NavNeedsDestination
+            if (hit.RigidBodyIndex == -1) return;
+
+            var hitSurfaceEntity = physicsWorld.Bodies[hit.RigidBodyIndex].Entity;
+
+            if (hitSurfaceEntity == Entity.Null) return;
+
+            if (!EntityManager.HasComponent<Parent>(agentEntity)) return;
+
+            var surfaceEntity = EntityManager.GetComponentData<Parent>(agentEntity).Value;
+
+            if (surfaceEntity == Entity.Null) return;
+
+            if (surfaceEntity == hitSurfaceEntity)
             {
-                Destination = hit.point,
-                Tolerance = 1
-            });
+                EntityManager.AddComponentData(agentEntity, new NavNeedsDestination
+                {
+                    Destination = hit.Position,
+                    Tolerance = 1
+                });
+
+                return;
+            }
+
+            if (!EntityManager.HasComponent<NavJumpableBufferElement>(surfaceEntity)) return;
+
+            var jumpableSurfaces = EntityManager.GetBuffer<NavJumpableBufferElement>(surfaceEntity);
+
+            for (var i = 0; i < jumpableSurfaces.Length; ++i)
+            {
+                if (hitSurfaceEntity == jumpableSurfaces[i])
+                {
+                    EntityManager.AddComponentData(agentEntity, new NavNeedsDestination
+                    {
+                        Destination = hit.Position,
+                        Tolerance = 1
+                    });
+
+                    break;
+                }
+            }
         }
     }
 }
