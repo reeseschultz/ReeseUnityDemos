118d84e5536e06c667521a4b48070f1f683aae92
Reese Schultz <reeseschultz@gmail.com>
2020-11-26 23:04:50 -0500

    Add hopping to cat in Stranded demo

--- Assets/Scripts/Stranded/CatSystem.cs
+++ Assets/Scripts/Stranded/CatSystem.cs
@@ -1,5 +1,7 @@
 ﻿using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +9,8 @@ namespace Reese.Demo
 {
     class CatSystem : SystemBase
     {
+        EntityCommandBufferSystem barrier => World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
         GameObject catGO = default;
 
         protected override void OnCreate()
@@ -24,19 +28,55 @@ namespace Reese.Demo
         {
             if (catGO == null) return;
 
+            var commandBuffer = barrier.CreateCommandBuffer();
+
+            var elapsedSeconds = (float)Time.ElapsedTime;
+
             Entities
+                .WithStructuralChanges()
                 .WithChangeFilter<SpatialEvent>()
-                .ForEach((in Cat cat) =>
+                .WithNone<Hopping>()
+                .ForEach((Entity entity, in Cat cat, in Translation translation) =>
                 {
                     var meowController = catGO.GetComponent<CatMeowController>();
 
                     if (meowController == null) return;
 
                     meowController.Meow();
+
+                    commandBuffer.AddComponent(entity, new Hopping
+                    {
+                        OriginalPosition = translation.Value,
+                        Height = 10,
+                        StartSeconds = elapsedSeconds,
+                        Duration = 1
+                    });
                 })
-                .WithoutBurst()
                 .WithName("MeowJob")
                 .Run();
+
+            var parallelCommandBuffer = commandBuffer.AsParallelWriter();
+
+            Entities
+                .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, in Hopping translator) =>
+                {
+                    var position = translation.Value;
+
+                    position.y = translator.Height * math.sin(
+                        math.PI * ((elapsedSeconds - translator.StartSeconds) / translator.Duration)
+                    );
+
+                    translation.Value = position;
+
+                    if (elapsedSeconds - translator.StartSeconds < translator.Duration) return;
+
+                    translation.Value = translator.OriginalPosition;
+                    parallelCommandBuffer.RemoveComponent<Hopping>(entityInQueryIndex, entity);
+                })
+                .WithName("CatTranslateJob")
+                .ScheduleParallel();
+
+            barrier.AddJobHandleForProducer(Dependency);
         }
     }
 }
