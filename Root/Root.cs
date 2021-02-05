6d800bae28a780819e1588fde18c92132fef4334
Reese Schultz <reeseschultz@gmail.com>
2021-02-05 13:03:42 -0500

    Add root component and system to utility package

---
+++ Root/Root.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+namespace Reese.Utility
+{
+    public struct Root : IComponentData
+    {
+        public Entity Value;
+    }
+}
---
+++ Root/RootSystem.cs
@@ -0,0 +1,32 @@
+using Unity.Entities;
+using Unity.Jobs;
+
+namespace Reese.Utility
+{
+    public class RootSystem : SystemBase
+    {
+        EntityCommandBufferSystem barrier => World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        protected override void OnUpdate()
+        {
+            var commandBuffer = barrier.CreateCommandBuffer().AsParallelWriter();
+
+            Entities
+                .WithChangeFilter<LinkedEntityGroup>()
+                .ForEach((Entity entity, int nativeThreadIndex, ref DynamicBuffer<LinkedEntityGroup> linkedEntities) =>
+                {
+                    for (var i = 0; i < linkedEntities.Length; ++i)
+                    {
+                        commandBuffer.AddComponent(nativeThreadIndex, linkedEntities[i].Value, new Root
+                        {
+                            Value = entity
+                        });
+                    }
+                })
+                .WithName("RootJob")
+                .ScheduleParallel();
+
+            barrier.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
