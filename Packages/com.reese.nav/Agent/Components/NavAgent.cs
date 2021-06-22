ece68dd1a9888ff2ccb385c66a1c1ac687b0e0f6
Dennis Weishaar <email.dweishaar@gmail.com>
2021-06-22 02:58:21 +0200

    added comments

--- Packages/com.reese.nav/Agent/Components/NavAgent.cs
+++ Packages/com.reese.nav/Agent/Components/NavAgent.cs
@@ -61,22 +61,29 @@ namespace Reese.Nav
         public float RotationSpeed;
 
         /// <summary>
-        ///  
+        /// The perception radius for the cohesion behavior. Differs from agent to agent because of body size. You should *probably* write
+        /// to this when spawning an agent and you want to use the flocking system.
+        /// Setting this to 1 or 2 is a good starting point.
         /// </summary>
         public float CohesionPerceptionRadius;
 
         /// <summary>
-        /// 
+        /// The percention radius for the alignment behavior. Differs from agent to agent because of body size. You should *probably* write
+        /// to this when spawning an agent and you want to use the flocking system.
+        /// Setting this to 1 or 2 is a good starting point.
         /// </summary>
         public float AlignmentPerceptionRadius;
         
         /// <summary>
-        /// 
+        /// The perception radius for the separation behavior. Keep it relatively low, it is the distance from where agents start to push each other away.
+        /// Differs from agent to agent because of body size. You should *probably* write
+        /// to this when spawning an agent and you want to use the flocking system.
         /// </summary>
         public float SeparationPerceptionRadius;
 
         /// <summary>
-        /// 
+        /// The distance from which agents start to steer away from each other. Differs from agent to agent because of body size.
+        /// You should *probably* write to this when spawning an agent and you want to use the flocking system.
         /// </summary>
         public float ObstacleAversionDistance;
 
