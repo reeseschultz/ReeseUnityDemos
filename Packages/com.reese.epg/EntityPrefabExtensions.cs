fb005536a73d12aeea8b2f744ee8df59e0ec5154
Reese Schultz <reeseschultz@gmail.com>
2021-09-20 11:59:45 -0400

    Add GetSingleton extension method to epg package

--- Packages/com.reese.epg/EntityPrefabExtensions.cs
+++ Packages/com.reese.epg/EntityPrefabExtensions.cs
@@ -7,16 +7,20 @@ namespace Reese.EntityPrefabGroups
         /// <summary>Gets a prefab with the specified singleton component type.</summary>
         public static Entity GetPrefab<T>(this EntityManager entityManager) where T : struct, IComponentData
             => entityManager.CreateEntityQuery(
-                typeof(T),
-                typeof(Prefab)
+                ComponentType.ReadOnly(typeof(T)),
+                ComponentType.ReadOnly(typeof(Prefab))
             ).GetSingletonEntity();
+
+        /// <summary>Gets the singleton data of the specified component type.</summary>
+        public static T GetSingleton<T>(this EntityManager entityManager) where T : struct, IComponentData
+            => entityManager.CreateEntityQuery(typeof(T)).GetSingleton<T>();
 
         /// <summary>Gets a buffer of prefabs with a group that has the specified singleton component type.</summary>
         public static DynamicBuffer<PrefabGroup> GetPrefabs<T>(this EntityManager entityManager) where T : struct, IComponentData
         {
             var group = entityManager.CreateEntityQuery(
-                typeof(T),
-                typeof(PrefabGroup)
+                ComponentType.ReadOnly(typeof(T)),
+                ComponentType.ReadOnly(typeof(PrefabGroup))
             ).GetSingletonEntity();
 
             return entityManager.GetBuffer<PrefabGroup>(group);
