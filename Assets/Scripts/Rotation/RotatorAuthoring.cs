cdda139bad7b3b101ffda70127ba880c6b4c32e7
Reese Schultz <contact@reese.codes>
2022-06-18 12:03:46 -0500

    Uncomment a script

--- Assets/Scripts/Rotation/RotatorAuthoring.cs
+++ Assets/Scripts/Rotation/RotatorAuthoring.cs
@@ -3,31 +3,25 @@ using UnityEngine;
 
 namespace Reese.Demo
 {
-    // TODO : not working with entities 0.51
-    // public class RotatorAuthoring : MonoBehaviour, IConvertGameObjectToEntity
-    // {
-        // [SerializeField]
-        // Vector3 fromRelativeAngles = new Vector3(0, 0, 0);
+    public class RotatorAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        [SerializeField]
+        Vector3 fromRelativeAngles = new Vector3(0, 0, 0);
 
-        // [SerializeField]
-        // Vector3 toRelativeAngles = new Vector3(0, 0, 0);
+        [SerializeField]
+        Vector3 toRelativeAngles = new Vector3(0, 0, 0);
 
-        // [SerializeField]
-        // float frequency = 1;
+        [SerializeField]
+        float frequency = 1;
 
-        // public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
-        // {
-        //     dstManager.AddComponentData(entity, new Rotator
-        //     {
-        //         FromRelativeAngles = fromRelativeAngles + transform.localRotation.eulerAngles,
-        //         ToRelativeAngles = toRelativeAngles + transform.localRotation.eulerAngles,
-        //         Frequency = frequency
-        //     });
-        // }
-
-        // public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
-        // {
-        //     throw new System.NotImplementedException();
-        // }
-    // }
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponentData(entity, new Rotator
+            {
+                FromRelativeAngles = fromRelativeAngles + transform.localRotation.eulerAngles,
+                ToRelativeAngles = toRelativeAngles + transform.localRotation.eulerAngles,
+                Frequency = frequency
+            });
+        }
+    }
 }
